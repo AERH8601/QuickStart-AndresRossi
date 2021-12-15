6e1b563400215ebbf952c23e0655d68b908f863c
AERH8601 <andres.rossi@udea.edu.co>
2021-12-14 22:12:57 -0500

    2 files modified

--- src/app/product-alerts/product-alerts.component.ts
+++ src/app/product-alerts/product-alerts.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Product } from '../products';
+
+export class ProductAlertsComponent {
+  @Input() product: Product | undefined;
+  @Output() notify = new EventEmitter();
+}
 
 @Component({
   selector: 'app-product-alerts',
@@ -13,6 +20,19 @@ export class ProductAlertsComponent implements OnInit {
   }
 
 }
+export class ProductListComponent {
+
+  products = products;
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
+
 import { Component, OnInit, Input } from '@angular/core';
 import { Product } from '../products';
 
